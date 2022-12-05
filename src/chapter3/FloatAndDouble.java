bb0ba1c3258645b4ebdfcc980a8b2c7207ddd200
Asif Jaman <asifuzzaman.opi@gmail.com>
2022-12-05 17:42:03 +0600

    First Steps

--- src/chapter3/FloatAndDouble.java
+++ src/chapter3/FloatAndDouble.java
@@ -22,3 +22,4 @@ public class FloatAndDouble {
         System.out.println(myDoubleValue);
     }
 }
+
--- src/chapter3/PrimitiveTypesChallenge.java
+++ src/chapter3/PrimitiveTypesChallenge.java
@@ -10,3 +10,4 @@ public class PrimitiveTypesChallenge {
         System.out.println(longVar);
     }
 }
+
